4327151cad90c01417b7ff295d88c0e2f3ff791b
Johnson <johnsonphan95@gmail.com>
2019-06-07 16:46:29 -0700

    fix wyr show and type

--- frontend/src/components/questions/question_show.jsx
+++ frontend/src/components/questions/question_show.jsx
@@ -116,15 +116,43 @@ class QuestionShow extends React.Component {
             )
         } else if (this.props.question.questionType === "rfdb") {
             return (
-                <div>
-                    <div>
-                        <h2>{this.props.question.body}</h2>
-                    </div>
-                    <div>
-                        <h4 className="rfdb-button" onClick={this.handleOption1}>Red Flag</h4>
-                    </div>
-                    <div>
-                        <h4 className="rfdb-button" onClick={this.handleOption2}>Deal Breaker</h4>
+                // <div>
+                //     <div>
+                //         <h2>{this.props.question.body}</h2>
+                //     </div>
+                //     <div>
+                //         <h4 className="rfdb-button" onClick={this.handleOption1}>Red Flag</h4>
+                //     </div>
+                //     <div>
+                //         <h4 className="rfdb-button" onClick={this.handleOption2}>Deal Breaker</h4>
+                //     </div>
+                // </div> 
+                // 
+                <div className="showbackground">
+                    <div className="question-container">
+                        <div className="container-header-parent">
+                            <div className="container-header">
+                                <div className="wyr-head">{this.props.question.body} </div>
+                                <div className="upvoteDownvote">
+                                    <div className="upvote-button" onClick={this.handleUpvote}>
+                                        <i className="likeEmote far fa-grin-hearts"></i> {this.props.question.upvote} <br />
+                                    </div>
+                                    <div className="downvote-button" onClick={this.handleDownvote}>
+                                        <i className="dislikeEmote far fa-flushed"></i> {this.props.question.downvote}
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="answers">
+                                    <div className="answer-button" onClick={this.handleOption1}>
+                                        Red Flag
+                                    </div>
+                                    <div className="answer-button" onClick={this.handleOption2}>
+                                        Dealbreaker
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )
