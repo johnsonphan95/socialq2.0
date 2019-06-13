065182dc3934260b16ac9272019c4e268c9c3013
Johnson <johnsonphan95@gmail.com>
2019-06-13 13:14:16 -0700

    fix keys file

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,3 @@
-// module.exports = {
-//   mongoURI: 'mongodb+srv://johnsonphan95:qrUJ8Z3dJVp2Ay13@cluster0-qaw0r.mongodb.net/test?retryWrites=true&w=majority',
-//   //Make sure this is your own unique string
-//   secretOrKey: 'secret'
-// }
 
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_prod');
